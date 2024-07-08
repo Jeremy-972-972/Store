1c1c0e8b8a9d91a7b95584c47f9ba295db5b6070
Jérémy <asjeremy83@gmail.com>
2024-07-08 11:39:34 -0400

    update model category and product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
     use HasFactory;
+    protected $fillable = ['category_id', 'name', 'description', 'price', ' images'];
+
+    /**
+     * Get the user that owns the Product
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class,);
+    }
 }
